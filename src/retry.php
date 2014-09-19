7712f165b392f089120190bdd0809baa911c487a
Márcio Almada <marcio3w@gmail.com>
2014-09-19 19:07:28 -0300

    add docblocks

--- src/retry.php
+++ src/retry.php
@@ -4,6 +4,13 @@ namespace igorw;
 
 class FailingTooHardException extends \Exception {}
 
+/**
+ * Try to execute some operation with retry limit
+ *
+ * @param  int|float  $retries maximum number of tries
+ * @param  callable   $fn      operation to be executed
+ * @throws \igorw\FailingTooHardException If maximum amount of $retries is exceeded
+ */
 function retry($retries, callable $fn)
 {
     beginning:
