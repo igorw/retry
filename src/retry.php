9a3abcced95ecbcc25745a4f310131c5ced1f6fb
Márcio Almada <marcio3w@gmail.com>
2014-09-19 19:11:16 -0300

    "simplify" code

--- src/retry.php
+++ src/retry.php
@@ -17,10 +17,9 @@ function retry($retries, callable $fn)
     try {
         return $fn();
     } catch (\Exception $e) {
-        if (!$retries) {
+        if (!$retries--) {
             throw new FailingTooHardException('', 0, $e);
         }
-        $retries--;
         goto beginning;
     }
 }
