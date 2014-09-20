e122fdc2f03b46c001b8de94363e3967f476d3bc
Igor Wiedler <igor@wiedler.ch>
2014-09-20 13:58:31 +0200

    allow providing an onError callable that is called on every error

--- src/retry.php
+++ src/retry.php
@@ -4,12 +4,15 @@ namespace igorw;
 
 class FailingTooHardException extends \Exception {}
 
-function retry($retries, callable $fn)
+function retry($retries, callable $fn, callable $onError = null)
 {
     beginning:
     try {
         return $fn();
     } catch (\Exception $e) {
+        if ($onError) {
+            $onError($e);
+        }
         if (!$retries) {
             throw new FailingTooHardException('', 0, $e);
         }
