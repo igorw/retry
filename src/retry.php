8f25d57b9083e62b22cc0383725a3884eec1a3c6
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-01-31 21:50:51 +0100

    :tada: we move the code outside of the catch block :
    line     #* E I O op                           fetch          ext  return  operands
    -------------------------------------------------------------------------------------
       3     0  E >   RECV                                             !0
             1        RECV                                             !1
             2        RECV_INIT                                        !2      null
       6     3    >   NOP
       7     4        INIT_DYNAMIC_CALL                                        !1
             5        DO_FCALL                                      0  $4
             6      > RETURN                                                   $4
             7*       JMP                                                      ->9
       8     8  E > > CATCH                                       last         'Exception'
      10     9    > > JMPZ                                                     !2, ->13
      11    10    >   INIT_DYNAMIC_CALL                                        !2
            11        SEND_VAR_EX                                              !3
            12        DO_FCALL                                      0
      13    13    >   BOOL_XOR                                         ~6      !0
            14      > JMPZ                                                     ~6, ->21
      14    15    >   NEW                                              $7      :18
            16        SEND_VAL_EX                                              ''
            17        SEND_VAL_EX                                              0
            18        SEND_VAR_EX                                              !3
            19        DO_FCALL                                      0
            20      > THROW                                         0          $7
      16    21    >   POST_DEC                                         ~9      !0
            22        FREE                                                     ~9
      17    23      > JMP                                                      ->3
      18    24*     > RETURN                                                   null

--- src/retry.php
+++ src/retry.php
@@ -9,15 +9,14 @@ function retry($retries, callable $fn, callable $onError = null)
     beginning:
     try {
         return $fn();
-    } catch (\Exception $e) {
-        if ($onError) {
-            $onError($e);
-        }
-        if (!$retries) {
-            throw new FailingTooHardException('', 0, $e);
-        }
-        $retries--;
-        goto beginning;
+    } catch (\Exception $e) {}
+    if ($onError) {
+        $onError($e);
     }
+    if (!$retries) {
+        throw new FailingTooHardException('', 0, $e);
+    }
+    $retries--;
+    goto beginning;
 }
 
